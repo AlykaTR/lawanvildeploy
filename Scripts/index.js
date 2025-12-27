4a2f0897ae216dcbdc1e77949bf9131e5fedf90d
Alyka Vidal <croscomedmovil@gmail.com>
2025-12-27 12:11:12 +0000

    added a simple popup

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,9 +1,17 @@
 const viewUniversesBtn = document.getElementById("viewUniversesButton");
+const createUniverseBtn = document.getElementById("createUniverseButton");
+
+const createUniversePopup = document.getElementById("createUniversePopup")
 
 viewUniversesBtn.addEventListener("click", viewUniverses);
+createUniverseBtn.addEventListener("click", createUniverse);
 
 function viewUniverses() {
     console.log("User clicked the view universes button");
+}
+
+function createUniverse(){
+    createUniversePopup.classList.remove("hidden");
 }
 
 console.log("index.js correctly loaded");
